5fb4acd61d057490c7e522392fd685199201a3e7
wdm64 <cyjwdm0503@foxmail.com>
2017-03-28 22:16:58 +0800

    补充上次没有传送完成的的文件

---
+++ protocol/contentprotocol.cpp
@@ -0,0 +1,70 @@
+#include "contentprotocol.h"
+#include "contentpackage.h"
+
+int CContentProtocol::Pop( CPackage* package )
+{
+	CContentPackage* contentpackage = dynamic_cast<CContentPackage*>(package);
+	if(contentpackage == NULL)
+		return -1;
+	if(contentpackage->GetActiveID() == CONTENTTYPENANO)
+	{//当前协议的报文
+		if(contentpackage->GetExtTag() == CONTENT_HEART_TAG)
+		{
+			set_timeout(package);
+		}
+		else if(contentpackage->GetExtTag() == CONTENT_ANSWER_TAG)
+		{
+			;
+		}
+	}
+	
+	//如果不是该层的报文，则线上进行Pop
+	return CProtocol::Pop(package);
+}
+
+int CContentProtocol::OnRecvErrPackage( CPackage* package )
+{
+	return CProtocol::OnRecvErrPackage(package);
+}
+
+int CContentProtocol::Push( CPackage* package,CProtocol* protocol )
+{
+	((CContentPackage*)m_sendPackage)->GetContentHeader()->Type =  package->GetActiveID();
+	return CProtocol::Push(package,protocol);
+}
+
+void CContentProtocol::OnTimer( int event )
+{
+	CProtocol::OnTimer(event);
+}
+
+CContentProtocol::CContentProtocol( CSelectReactor* reactor ):CProtocol(reactor,new CContentPackage(),new CContentPackage(),CONTENTHEADLENGTH+CONTENTEXTHEADLENGTH+MAX_CONTENT_LEN)
+{
+	m_timeOut = 0;
+}
+
+CContentProtocol::~CContentProtocol()
+{
+
+}
+
+void CContentProtocol::send_timeout( int timeout )
+{
+	CContentPackage sendpackage;
+	sendpackage.ConstructAlloc(0,GetReserverLen());
+	sendpackage.SetExtHeader(CONTENT_HEART_TAG,sizeof(int),(char*)&timeout);
+	send(&sendpackage);
+}
+
+void CContentProtocol::set_timeout( CPackage* package )
+{
+	CContentPackage* contentpkg = dynamic_cast<CContentPackage*>(package);
+	if(contentpkg == NULL)
+		return;
+	if(contentpkg->GetExtTag() != CONTENT_HEART_TAG)
+		return ;
+	if(contentpkg->GetExtTagLen() != sizeof(m_timeOut))
+		return;
+	memcpy(&m_timeOut,contentpkg->GetExtData(),sizeof(m_timeOut));
+}
+
---
+++ protocol/contentprotocol.h
@@ -0,0 +1,47 @@
+/************************************************************************/
+/* 
+具有一个完整package的protocol 
+此成会维护对应的contentpackage.同提供给上层应用层的接口;
+主要是上层协议XXXXProtocol利用Protocol.Send()写入到CContentProtocol层。
+CContentProtocol.Push()会写入到CChannelProtocol
+进行缓存，或者直接发送。
+
+见contentprotocol的send与push的描述
+
+*/
+/************************************************************************/
+
+#ifndef CONTENTPROTOCOL_H 
+#define CONTENTPROTOCOL_H
+
+#include "Protocol.h"
+
+
+class CContentProtocol:public CProtocol
+{
+public:
+	CContentProtocol(CSelectReactor* reactor);
+	virtual ~CContentProtocol();
+
+	virtual int Pop( CPackage* package );
+
+	virtual int OnRecvErrPackage( CPackage* package );
+
+	//向channelprotocol传送package时，添加content层的头部。
+	virtual int Push( CPackage* package,CProtocol* protocol );
+
+	virtual void OnTimer( int event );
+	
+	//发送timeout到对方，进行设置
+	void send_timeout(int timeout);
+
+	//利用收到的CContentPackage设置对应的心跳超时时间
+	void set_timeout(CPackage* package);
+
+private:
+	int m_timeOut;
+
+};
+
+
+#endif
